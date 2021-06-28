d5eea10e0e324ffcd1af626ae7630b05382bd1ab
TanvirAhmed1 <tanvirnoor38@yahoo.com>
2021-06-29 02:01:44 +0600

    junit5 unit test added

--- src/test/java/stack/StringStackArrayTest.java
+++ src/test/java/stack/StringStackArrayTest.java
@@ -8,7 +8,24 @@ class StringStackArrayTest {
 
 	@Test
 	void test() {
-		fail("Not yet implemented");
+		StringStackArray sas = new StringStackArray();
+		sas.push("Woafi");
+		sas.push("Tanvir");
+		String expected = "Tanvir";
+		String expected2 = "Woafi";
+		String actual = sas.pop();
+		String actual2 = sas.pop();
+		assertEquals(expected, actual);
+		assertEquals(expected2, actual2);
+		StringStackArray sas2 = new StringStackArray();
+		sas2.push("Shaha");
+		sas2.push("Afroza");
+		String expectedpeek = "Afroza";
+		String expectedpeek2 = "Shaha";
+		String actualpeek = sas2.peek();
+		String actualpeek2 = sas2.peek();
+		assertEquals(expectedpeek, actualpeek);
+		assertEquals(expectedpeek2, actualpeek2);
 	}
 
 }
